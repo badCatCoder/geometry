06ed71f53dfd36b48371451223a15a9f42cb7301
badCatCoder <badCatCoder@gmail.com>
2020-04-05 16:01:31 +0200

    Creato collegamento tra gui e codice

--- src/main/java/it/polito/tdp/geometry/FXMLController.java
+++ src/main/java/it/polito/tdp/geometry/FXMLController.java
@@ -4,22 +4,66 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private Label txtBase;
+
+    @FXML
+    private TextField in_base;
+
+    @FXML
+    private Label txtAltezza;
+
+    @FXML
+    private TextField in_altezza;
+
+    @FXML
+    private Button btnPerimetro;
+
+    @FXML
+    private Label lblPerimetro;
+
+    @FXML
+    private Button btnArea;
+
+    @FXML
+    private Label lblArea;
+
+    @FXML
+    private Label lblError;
+
+    @FXML
+    void handlerCalcolaArea(ActionEvent event) {
+
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    @FXML
+    void handlerCalcolaPerimetro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtBase != null : "fx:id=\"txtBase\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert in_base != null : "fx:id=\"in_base\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtAltezza != null : "fx:id=\"txtAltezza\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert in_altezza != null : "fx:id=\"in_altezza\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnPerimetro != null : "fx:id=\"btnPerimetro\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblPerimetro != null : "fx:id=\"lblPerimetro\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnArea != null : "fx:id=\"btnArea\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblArea != null : "fx:id=\"lblArea\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblError != null : "fx:id=\"lblError\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
